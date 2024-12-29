f5f57a3ab524bd943c66a9e8f79b23ec1f2421b3
zh1995 <rwzsjfm@qq.com>
2024-12-29 20:09:17 +0800

    add sds.c note

--- versions/redis-0.091/sds.h
+++ versions/redis-0.091/sds.h
@@ -28,20 +28,29 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+// 如果未定义__SDS_H宏，则定义__SDS_H宏。
+// 用于防止头文件的重复包含，防止编译出错
 #ifndef __SDS_H
 #define __SDS_H
 
+// 该头文件提供了必要的和系统相关的数据类型和结构定义
 #include <sys/types.h>
 
+// 使用typedef关键字给指向字符类型的指针（char *）定义一个别名sds
 typedef char *sds;
 
+// SDS header，即简单动态字符串结构的头部信息
 struct sdshdr {
-    long len;
-    long free;
-    char buf[];
+    long len;    // 存储字符串的实际长度
+    long free;   // 存储可用的额外空间
+    char buf[];  // 存储字符串的字符数组
 };
 
+// 形参前加上const关键字，表示该参数是只读的，函数内部无法修改它指向的内容
+// void *是一个指向任意类型的指针
+// size_t是一个无符号整数类型，用于表示对象的大小或数组的索引
 sds sdsnewlen(const void *init, size_t initlen);
+// char *表明该函数接受的是一个字符串输入，而sds的语义更倾向于表示一个动态字符串的封装结构
 sds sdsnew(const char *init);
 sds sdsempty();
 size_t sdslen(const sds s);
@@ -52,11 +61,13 @@ sds sdscatlen(sds s, void *t, size_t len);
 sds sdscat(sds s, char *t);
 sds sdscpylen(sds s, char *t, size_t len);
 sds sdscpy(sds s, char *t);
+// 省略号表示该函数可以接受可变数量的参数
 sds sdscatprintf(sds s, const char *fmt, ...);
 sds sdstrim(sds s, const char *cset);
 sds sdsrange(sds s, long start, long end);
 void sdsupdatelen(sds s);
 int sdscmp(sds s1, sds s2);
+// 返回一个指向字符串指针的指针数组
 sds *sdssplitlen(char *s, int len, char *sep, int seplen, int *count);
 void sdstolower(sds s);
 
